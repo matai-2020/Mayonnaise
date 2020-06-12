f7b6e755ffa46c064e447f9fb1db9440bdd46af5
Ariel <33848430+arielwc@users.noreply.github.com>
2020-06-12 13:31:06 +1200

    updated profile page

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const routes = require('./routes')
 const path = require('path')
 
 const server = express()
-const staticFolder = path.join(__dirname, 'public')
+
 server.engine('hbs', hbs({ defaultLayout: 'main', extname: 'hbs' }))
 server.set('view engine', 'hbs')
-server.use(express.static(staticFolder))
+server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
 
 server.use('/', routes)
